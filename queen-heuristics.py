8978439d67d195896e78dc29483b5184587c3ae7
crisdmej <72276529+crisdmej@users.noreply.github.com>
2020-10-26 08:38:51 -0700

    Add files via upload

---
+++ queen-heuristics.py
@@ -0,0 +1,132 @@
+from queen import Queen
+import pygame
+from pygame.locals import *
+
+foundDuplicate = False
+
+board = [[0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0],
+]
+
+heuristicBoard = [[0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0],
+]
+
+def printBoard(gameBoard):
+    for row in gameBoard:
+        print(row)
+
+#master list to hold all queen objects
+queens = [Queen(4,0,1), Queen(5,1,2), Queen(6,2,3), Queen(3,3,4), Queen(4,4,5), Queen(5,5,6), Queen(6,6,7), Queen(5,7,8)]
+verifiedHits = []
+
+#place all queens in the appropriate spot
+for i in range(len(queens)):
+    board[queens[i].getRow()][queens[i].getCol()] = queens[i].getNum()
+
+printBoard(board)
+
+#get the hit list for each queen
+#compare each item of the queen hit list
+#if that hit, reversed, is not in the list, add the forwards version
+
+lastRPos = queens[0].originalRow
+lastCPos = queens[0].originalCol
+
+#to iterate over every square
+for column in range(8):
+    #reset last column's piece
+    if(column != 0):
+        board[7][column-1] = 0
+        board[queens[column-1].getOriginalRow()][queens[column-1].getOriginalCol()] = column
+        queens[column-1].setPosition(queens[column-1].getOriginalRow(),
+                                        queens[column-1].getOriginalCol()) #rowPos, colPos is fixed
+
+    lastRPos = queens[column].originalRow #last known position of current column was original spot
+    lastCPos = queens[column].originalCol
+    
+    for row in range(8):
+        verifiedHits = []#reset verifiedHits for the new run
+        queens[column].setPosition(row, column) #queen object now knows their new spot
+        board[row][column] = column+1 #set the new piece in board
+        board[lastRPos][lastCPos] = 0 #unset the last piece's position
+        
+        #getSquareVal()...............................................................................
+        heuristic = 0
+        for j in range(len(queens)): #for the entire queen list, find the hits
+            queenHitList = []
+            queenHitList = queens[j].findHitPairs(board) #might need reinitialization
+            heuristic = len(queenHitList) + heuristic
+        #..................................................................................................
+        heuristicBoard[row][column] = heuristic
+
+        lastRPos = row
+        lastCPos = column
+
+print("\n")
+printBoard(board)
+print("\n")
+printBoard(heuristicBoard)
+
+#https://stackoverflow.com/questions/45945254/make-a-88-chessboard-in-pygame-with-python
+
+#3b: Make a prettier version of the output
+pygame.init()
+S_WIDTH = 700
+S_LENGTH = 700
+
+screen = pygame.display.set_mode((S_WIDTH, S_LENGTH))
+pygame.display.set_caption("Chessboard")
+
+size = 70
+lb = (209, 196, 132)
+db = (122, 105, 18)
+black = (0,0,0)
+white= (255,255,255)
+
+#board length, must be even
+boardLength = 8
+screen.fill((255,255,255))
+
+text = pygame.font.Font(None, 36)
+queenImage = pygame.image.load('queen.png')
+
+cnt = 0
+for i in range(1,boardLength+1):
+    for z in range(1,boardLength+1):
+        #check if current loop value is even
+        if cnt % 2 == 0:
+            pygame.draw.rect(screen, lb,[size*z,size*i,size,size])
+        else:
+            pygame.draw.rect(screen, db, [size*z,size*i,size,size])
+
+        number = text.render(str(heuristicBoard[i-1][z-1]), True, black)
+        screen.blit(number,(size*z+5,size*i+5))
+
+        for queensToDraw in range(len(queens)):
+            qRow = queens[queensToDraw].getOriginalRow()
+            qCol = queens[queensToDraw].getOriginalCol()
+
+            if(i-1 == qRow and z-1 == qCol):
+                screen.blit(queenImage, (size*z+30,size*i+30))
+        cnt +=1
+    #since theres an even number of squares go back one value
+    #screen.blit(number,(size*i,size*z))
+    cnt-=1
+#Add a nice boarder
+pygame.draw.rect(screen,black,[size,size,boardLength*size,boardLength*size],1)
+
+#while(1):
+#    pygame.display.update()
---
+++ queen.py
@@ -0,0 +1,86 @@
+class Queen:
+    def __init__(self, r, c, n):
+        self.rowPos = r
+        self.colPos = c
+        self.number = n
+        self.originalRow = r
+        self.originalCol = c
+
+    def getRow(self):
+        return self.rowPos
+
+    def getCol(self):
+        return self.colPos
+
+    def getNum(self):
+        return self.number
+
+    def getOriginalRow(self):
+        return self.originalRow
+
+    def getOriginalCol(self):
+        return self.originalCol
+
+    def setPosition(self, r, c):
+        self.rowPos = r
+        self.colPos = c
+
+    def findHitPairs(self, board):
+        possibleHits = []
+        downLeft = False
+        upLeft = False
+        upRight = False
+        downRight = False
+
+        diagR = 0
+        diagC = 0
+
+        for i in range(self.colPos, 8):
+            #horizontal motion
+            if(board[self.rowPos][i] != 0):
+                #print("Found a queen on the horizontal at position (" + str(self.rowPos) + "," + str(i) + ")\n")
+
+                toAdd = tuple((self.number,board[self.rowPos][i])) #tuple is the queen hitting, and queen getting hit
+
+                if(toAdd != (self.number, self.number)):
+                    possibleHits.append(toAdd)
+        
+        #yes, I know these diagonal moves are inefficient and bad
+
+        #downward right diagonal
+        diagR = self.rowPos
+        diagC = self.colPos
+        while(not downRight):
+            if(board[diagR][diagC] != 0):
+                #print("Found a queen on the down right diagonal at position (" + str(diagR) + "," + str(diagC) + ")\n")
+
+                toAdd = tuple((self.number,board[diagR][diagC]))
+
+                if(toAdd != (self.number, self.number)):
+                    possibleHits.append(toAdd)
+
+            if(diagC == 7 or diagR == 7):
+                downRight = True
+            else:
+                diagR = diagR + 1
+                diagC = diagC + 1
+
+        #upward right diagonal
+        diagR = self.rowPos
+        diagC = self.colPos
+        while(not upRight):
+            if(board[diagR][diagC] != 0):
+                #print("Found a queen on the upper right diagonal at position (" + str(diagR) + "," + str(diagC) + ")\n")
+
+                toAdd = tuple((self.number,board[diagR][diagC]))
+
+                if(toAdd != (self.number, self.number)):
+                    possibleHits.append(toAdd)
+
+            if(diagC == 7 or diagR == 0):
+                upRight = True
+            else:
+                diagR = diagR - 1
+                diagC = diagC + 1
+
+        return possibleHits
