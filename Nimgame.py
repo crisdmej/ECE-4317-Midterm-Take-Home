61f45be0adece0d77002c8a277d47ab9aff315e3
crisdmej <72276529+crisdmej@users.noreply.github.com>
2020-10-26 12:39:36 -0700

    Add files via upload

---
+++ Nimgame.py
@@ -0,0 +1,23 @@
+#Nim game 
+
+win = False;
+xor = 0
+exception = 0
+heap = input("Please enter the size of the heap: ")
+arr = []
+for x in range(0, int(heap)):
+    sticks = input("Enter the number of sticks in heap " + str(x+1) + ": ")
+    arr.append(int(sticks))
+for stick in list(arr):
+    xor = xor ^ stick
+    exception = exception + stick
+print(arr)
+if (xor != 0):
+    win = True
+if (exception == 1):
+    win = False
+if (win == False):
+    print("This is a losing hand!")
+else:
+    print("This is a winning hand!")
+
